7ae37d6eae1e27c43a1964b6ed0d8ece62a186d9
lassepe <lasse.peters@mailbox.org>
2024-12-05 14:05:40 +0100

    Don't use weakdeps, use strong deps for now

--- src/SymbolicTracingUtils.jl
+++ src/SymbolicTracingUtils.jl
@@ -6,6 +6,9 @@ also settings where tracing is performed merely to generate an efficient impleme
 """
 module SymbolicTracingUtils
 
+using Symbolics: Symbolics
+using FastDifferentiation: FastDifferentiation as FD
+
 export SymbolicsBackend, FastDifferentiationBackend, make_variables, build_function
 
 struct SymbolicsBackend end
@@ -18,6 +21,20 @@ Creates a vector of `dimension` where each element is a scalar symbolic variable
 """
 function make_variables end
 
+function make_variables(::SymbolicsBackend, name::Symbol, dimension::Int)
+    vars = Symbolics.@variables($name[1:dimension]) |> only |> Symbolics.scalarize
+
+    if isempty(vars)
+        vars = Symbolics.Num[]
+    end
+
+    vars
+end
+
+function make_variables(::FastDifferentiationBackend, name::Symbol, dimension::Int)
+    FD.make_variables(name, dimension)
+end
+
 """
     build_function(backend, f_symbolic, args_symbolic...; in_place, options)
 
@@ -29,12 +46,46 @@ Depending on the `in_place` flag, the function will be built as in-place `f!(res
 """
 function build_function end
 
+function build_function(
+    f_symbolic::AbstractArray{T},
+    args_symbolic...;
+    in_place,
+    backend_options=(;),
+) where {T<:Symbolics.Num}
+    f_callable, f_callable! = Symbolics.build_function(
+        f_symbolic,
+        args_symbolic...;
+        expression=Val{false},
+        # slightly saner defaults...
+        (; parallel=Symbolics.ShardedForm(), backend_options...)...,
+    )
+    in_place ? f_callable! : f_callable
+end
+
+function build_function(
+    f_symbolic::AbstractArray{T},
+    args_symbolic...;
+    in_place,
+    backend_options=(;),
+) where {T<:FD.Node}
+    FD.make_function(f_symbolic, args_symbolic...; in_place, backend_options...)
+end
+
 """
     gradient(f_symbolic, x_symbolic)
 
 Computes the symbolic gradient of `f_symbolic` with respect to `x_symbolic`.
 """
 function gradient end
+
+function gradient(f_symbolic::T, x_symbolic::Vector{T}) where {T<:Symbolics.Num}
+    Symbolics.gradient(f_symbolic, x_symbolic)
+end
+
+function gradient(f_symbolic::T, x_symbolic::Vector{T}) where {T<:FD.Node}
+    # FD does not have a gradient utility so we just flatten the jacobian here
+    vec(FD.jacobian([f_symbolic], x_symbolic))
+end
 
 """
     jacobian(f_symbolic, x_symbolic)
@@ -43,11 +94,27 @@ Computes the symbolic Jacobian of `f_symbolic` with respect to `x_symbolic`.
 """
 function jacobian end
 
+function jacobian(f_symbolic::Vector{T}, x_symbolic::Vector{T}) where {T<:Symbolics.Num}
+    Symbolics.jacobian(f_symbolic, x_symbolic)
+end
+
+function jacobian(f_symbolic::Vector{T}, x_symbolic::Vector{T}) where {T<:FD.Node}
+    FD.jacobian([f_symbolic], x_symbolic)
+end
+
 """
     sparse_jacobian(f_symbolic, x_symbolic)
 
 Computes the symbolic Jacobian of `f_symbolic` with respect to `x_symbolic` in a sparse format.
 """
 function sparse_jacobian end
+
+function sparse_jacobian(f_symbolic::Vector{T}, x_symbolic::Vector{T}) where {T<:Symbolics.Num}
+    Symbolics.sparsejacobian(f_symbolic, x_symbolic)
+end
+
+function sparse_jacobian(f_symbolic::Vector{T}, x_symbolic::Vector{T}) where {T<:FD.Node}
+    FD.sparse_jacobian(f_symbolic, x_symbolic)
+end
 
 end # module SymbolicTracingUtils
